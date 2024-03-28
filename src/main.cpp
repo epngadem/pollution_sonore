a97ee095e128d18e3bdd27cb0befb13d4ab0e916
epngadem <162624680+epngadem@users.noreply.github.com>
2024-03-28 03:13:48 -0300

    ajustement de la page html pour la reception des donnees

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include "../lib/CapteurSonore.h"
 #include "../lib/ServeurHTTP.h"
 
-const char* ssid = "Ariane";
-const char* password = "canada2023";
+const char* ssid = "Ornella KOM";
+const char* password = "Ornella2023";
 const int pinCapteurSonore = 32;
 
 CapteurSonore capteur(pinCapteurSonore);
